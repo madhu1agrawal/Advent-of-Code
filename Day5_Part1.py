bd9b7163b6688fe0af49dd202043bad7780ac6af
Madhu Agrawal <madhu1agrawal@gmail.com>
2022-12-22 16:46:31 +0000

    Create Day5_Part1.py

---
+++ Day5_Part1.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+with open('input5.txt') as f:
+    lines = f.readlines()
+    
+stacks = []
+for line in lines:
+    list1 = []
+    if line.strip():
+        line = line.replace('\n','').replace('    ',' 0 ').replace('[','').replace(']','').replace(' ','')
+        for x in line:
+            list1.append(x)  
+        stacks.append(list1)               
+    else:
+        break
+stacks = np.array(stacks).T
+stacks = np.flip(stacks, axis=1).tolist()
+stacks = {row[0]: row[1:] for row in stacks}
+for key in stacks:
+    while '0' in stacks[key]:
+        stacks[key].pop()
+
+count=0
+with open('input5.txt') as f:
+    lines = f.readlines()
+    
+    for line in lines:
+        if count <= len(stacks):
+            count+=1
+        else:
+            number = line.strip().split(' ')[1]
+            source = line.strip().split(' ')[3]
+            dest = line.strip().split(' ')[5]
+            
+            for crates in range(1,int(number)+1):
+                move_crate = stacks[source].pop()
+                stacks[dest].append(move_crate)
+    output = ''           
+    for key in stacks:
+        output = output + stacks[key].pop()
+print(output)
