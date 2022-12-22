221df6c59e7e75cb0502d9b505f36e6976c308f3
Madhu Agrawal <madhu1agrawal@gmail.com>
2022-12-22 16:45:38 +0000

    Create Day4_Part1.py

---
+++ Day4_Part1.py
@@ -0,0 +1,20 @@
+count = 0
+with open('input4.txt') as f:
+    while True:
+        line = f.readline().strip()
+
+        if not line:
+            break
+        l1 = int(line.split(',')[0].split('-')[0])
+        u1 = int(line.split(',')[0].split('-')[1])
+
+        l2 = int(line.split(',')[1].split('-')[0])
+        u2 = int(line.split(',')[1].split('-')[1])
+
+        set1 = set(range(l1, u1+1))
+        set2 = set(range(l2, u2+1))
+        
+        if (set1 <= set2) or (set2 <= set1):
+            count += 1
+            
+print(count)
